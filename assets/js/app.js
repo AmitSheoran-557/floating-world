a1a769e36d5995dbfbfbd40df6dc92c53f4c8cb7
AmitSheoran-557 <amit22082006@gmail.com>
2025-04-03 15:49:53 +0530

    add back to top btn

--- assets/js/app.js
+++ assets/js/app.js
@@ -59,3 +59,8 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+// ============================ back to top button js ============================
+
+let topBtn = document.querySelector(".top-btn");
+topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+window.onscroll = () => (topBtn.style.opacity = window.scrollY > 150 ? 1 : 0);
